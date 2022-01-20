390314e19c4a6c02dfb7eb907ddf395171b1dabf
parzivail <colby.newman2000@gmail.com>
2022-01-20 12:28:48 -0500

    convert TopocentricObservation to a record

--- SGP.NET/Observation/GroundStation.cs
+++ SGP.NET/Observation/GroundStation.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading;
 using SGPdotNET.CoordinateSystem;
 using SGPdotNET.Util;
 
--- SGP.NET/Observation/TopocentricObservation.cs
+++ SGP.NET/Observation/TopocentricObservation.cs
@@ -5,152 +5,73 @@ using SGPdotNET.Util;
 
 namespace SGPdotNET.Observation
 {
-    /// <summary>
-    ///     Stores a topocentric location (azimuth, elevation, range and range rate).
-    /// </summary>
-    public class TopocentricObservation
-    {
-        /// <summary>
-        ///     Azimuth relative to the observer
-        /// </summary>
-        public Angle Azimuth { get; }
+	/// <summary>
+	///     Stores a topocentric location (azimuth, elevation, range and range rate).
+	/// </summary>
+	/// <param name="Azimuth">Azimuth relative to the observer</param>
+	/// <param name="Elevation">Elevation relative to the observer</param>
+	/// <param name="Range">Range relative to the observer, in kilometers</param>
+	/// <param name="RangeRate">Range rate relative to the observer, in kilometers/second</param>
+	/// <param name="ReferencePosition">The position from which the satellite was observed to generate this observation</param>
+	public record TopocentricObservation(Angle Azimuth, Angle Elevation, double Range, double RangeRate, Coordinate ReferencePosition = null)
+	{
+		/// <summary>
+		///     Direction relative to the observer
+		/// </summary>
+		public RelativeDirection Direction => GetRelativeDirection();
 
-        /// <summary>
-        ///     Elevation relative to the observer
-        /// </summary>
-        public Angle Elevation { get; }
+		/// <summary>
+		///     Time for an ideal radio signal to travel the distance between the observer and the satellite, in seconds
+		/// </summary>
+		public double SignalDelay => GetSignalDelay();
 
-        /// <summary>
-        ///     Range relative to the observer, in kilometers
-        /// </summary>
-        public double Range { get; }
+		/// <summary>
+		///     Creates a new topocentric coordinate at the origin
+		/// </summary>
+		public TopocentricObservation() : this(Angle.Zero, Angle.Zero, 0, 0)
+		{
+		}
 
-        /// <summary>
-        ///     Range rate relative to the observer, in kilometers/second
-        /// </summary>
-        public double RangeRate { get; }
+		/// <summary>
+		///     Creates a new topocentric coordinate as a copy of the specified one
+		/// </summary>
+		/// <param name="topo">Object to copy from</param>
+		public TopocentricObservation(TopocentricObservation topo)
+		{
+			Azimuth = topo.Azimuth;
+			Elevation = topo.Elevation;
+			Range = topo.Range;
+			RangeRate = topo.RangeRate;
+			ReferencePosition = topo.ReferencePosition;
+		}
 
-        /// <summary>
-        ///     The position from which the satellite was observed to generate this observation
-        /// </summary>
-        public Coordinate ReferencePosition { get; }
+		private double GetSignalDelay()
+		{
+			return SgpConstants.SpeedOfLight / (Range * SgpConstants.MetersPerKilometer);
+		}
 
-        /// <summary>
-        ///     Direction relative to the observer
-        /// </summary>
-        public RelativeDirection Direction => GetRelativeDirection();
+		private RelativeDirection GetRelativeDirection()
+		{
+			if (Math.Abs(RangeRate) < double.Epsilon) return RelativeDirection.Fixed;
+			return RangeRate < 0 ? RelativeDirection.Approaching : RelativeDirection.Receding;
+		}
 
-        /// <summary>
-        ///     Time for an ideal radio signal to travel the distance between the observer and the satellite, in seconds
-        /// </summary>
-        public double SignalDelay => GetSignalDelay();
+		/// <summary>
+		///     Predicts the doppler shift of the satellite relative to the observer, in Hz
+		/// </summary>
+		/// <param name="inputFrequency">The base RX/TX frequency, in Hz</param>
+		/// <returns>The doppler shift of the satellite</returns>
+		public double GetDopplerShift(double inputFrequency)
+		{
+			var rr = RangeRate * SgpConstants.MetersPerKilometer;
+			return -rr / SgpConstants.SpeedOfLight * inputFrequency;
+		}
 
-        /// <summary>
-        ///     Creates a new topocentric coordinate at the origin
-        /// </summary>
-        public TopocentricObservation()
-        {
-        }
-
-        /// <summary>
-        ///     Creates a new topocentric coordinate with the specified values
-        /// </summary>
-        /// <param name="azimuth">Azimuth</param>
-        /// <param name="elevation">Elevation</param>
-        /// <param name="range">Range in kilometers</param>
-        /// <param name="rangeRate">Range rate in kilometers/second</param>
-        /// <param name="referencePosition">The position from which the satellite was observed to generate this observation</param>
-        public TopocentricObservation(Angle azimuth, Angle elevation, double range, double rangeRate,
-            Coordinate referencePosition = null)
-        {
-            Azimuth = azimuth;
-            Elevation = elevation;
-            Range = range;
-            RangeRate = rangeRate;
-            ReferencePosition = referencePosition;
-        }
-
-        /// <summary>
-        ///     Creates a new topocentric coordinate as a copy of the specified one
-        /// </summary>
-        /// <param name="topo">Object to copy from</param>
-        public TopocentricObservation(TopocentricObservation topo)
-        {
-            Azimuth = topo.Azimuth;
-            Elevation = topo.Elevation;
-            Range = topo.Range;
-            RangeRate = topo.RangeRate;
-            ReferencePosition = topo.ReferencePosition;
-        }
-
-        private double GetSignalDelay()
-        {
-            return SgpConstants.SpeedOfLight / (Range * SgpConstants.MetersPerKilometer);
-        }
-
-        private RelativeDirection GetRelativeDirection()
-        {
-            if (Math.Abs(RangeRate) < double.Epsilon) return RelativeDirection.Fixed;
-            return RangeRate < 0 ? RelativeDirection.Approaching : RelativeDirection.Receding;
-        }
-
-        /// <summary>
-        ///     Predicts the doppler shift of the satellite relative to the observer, in Hz
-        /// </summary>
-        /// <param name="inputFrequency">The base RX/TX frequency, in Hz</param>
-        /// <returns>The doppler shift of the satellite</returns>
-        public double GetDopplerShift(double inputFrequency)
-        {
-            var rr = RangeRate * SgpConstants.MetersPerKilometer;
-            return -rr / SgpConstants.SpeedOfLight * inputFrequency;
-        }
-
-        /// <inheritdoc />
-        protected bool Equals(TopocentricObservation other)
-        {
-            return Azimuth.Equals(other.Azimuth) && Elevation.Equals(other.Elevation) && Range.Equals(other.Range) &&
-                   RangeRate.Equals(other.RangeRate) && ReferencePosition.Equals(other.ReferencePosition);
-        }
-
-        /// <inheritdoc />
-        public override bool Equals(object obj)
-        {
-            if (obj is null) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            return obj is TopocentricObservation top && Equals(top);
-        }
-
-        /// <inheritdoc />
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hashCode = Azimuth.GetHashCode();
-                hashCode = (hashCode * 397) ^ Elevation.GetHashCode();
-                hashCode = (hashCode * 397) ^ Range.GetHashCode();
-                hashCode = (hashCode * 397) ^ RangeRate.GetHashCode();
-                hashCode = (hashCode * 397) ^ ReferencePosition.GetHashCode();
-                return hashCode;
-            }
-        }
-
-        /// <inheritdoc />
-        public static bool operator ==(TopocentricObservation left, TopocentricObservation right)
-        {
-            return Equals(left, right);
-        }
-
-        /// <inheritdoc />
-        public static bool operator !=(TopocentricObservation left, TopocentricObservation right)
-        {
-            return !Equals(left, right);
-        }
-
-        /// <inheritdoc />
-        public override string ToString()
-        {
-            return
-                $"TopocentricObservation[Azimuth={Azimuth}, Elevation={Elevation}, Range={Range}km, RangeRate={RangeRate}km/s]";
-        }
-    }
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			return
+				$"TopocentricObservation[Azimuth={Azimuth}, Elevation={Elevation}, Range={Range}km, RangeRate={RangeRate}km/s]";
+		}
+	}
 }
